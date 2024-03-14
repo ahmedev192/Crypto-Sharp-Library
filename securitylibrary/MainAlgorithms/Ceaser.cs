89eb6a71c7d0304c2d04460df0561a0658dcf905
Ahmed Mahmoud <ahmedev192@gmail.com>
2024-03-14 14:42:48 +0200

    Ceaser Cipher

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,17 +11,63 @@ namespace SecurityLibrary
     {
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToUpper();
+            string CYPHERTEXT = "";
+            //loop on each character of the plaintext to convert every letter to ascii 
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                char ch = plainText[i];
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    int numOfChar = ch - 'A';
+                    int ASCII = ((numOfChar + key) % 26);
+                    char encryptedCH = (char)(ASCII + 'A');
+                    CYPHERTEXT += encryptedCH;
+                }
+                else
+                {
+                    //if the letter is number
+                    CYPHERTEXT += ch;
+                }
+            }
+            return CYPHERTEXT;
+            //  throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            cipherText.ToUpper();
+            string PLAINTEXT = "";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                char ch = cipherText[i];
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    int numOfChar = ch - 'A';
+                    int ASCII = ((numOfChar - key + 26) % 26);
+                    char decryptedCH = (char)(ASCII + 'A');
+                    PLAINTEXT += decryptedCH;
+                }
+                else
+                {
+                    PLAINTEXT += ch;
+                }
+            }
+            return PLAINTEXT;
+            // throw new NotImplementedException();
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToUpper();
+            cipherText = cipherText.ToUpper();
+            int key = cipherText[0] - plainText[0];
+            if (key < 0)
+            {
+                key += 26;
+            }
+            return key;
+            //  throw new NotImplementedException();
         }
     }
 }
